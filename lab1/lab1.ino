4deeeec11dcf261ac0f7604abb4d8bd4bd355c22
lgnv <43182296+lgnv@users.noreply.github.com>
2019-11-09 12:04:44 +0500

    Laboratory work completed 1

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,66 +1,50 @@
 #include <Arduino.h>
-#include <MD_TCS230.h>
+const int countLed = 8;
+const int startPin = 2;
+const int endPin = startPin + countLed;
 
-#define  S0_OUT  2
-#define  S1_OUT  3
-#define  S2_OUT  4
-#define  S3_OUT  5
-
-#define R_OUT 6
-#define G_OUT 7
-#define B_OUT 8
-
-MD_TCS230 colorSensor(S2_OUT, S3_OUT, S0_OUT, S1_OUT);
-
-void setup()
-{
+void setup(){
     Serial.begin(115200);
     Serial.println("Started!");
-
-    sensorData whiteCalibration;
-    whiteCalibration.value[TCS230_RGB_R] = 0;
-    whiteCalibration.value[TCS230_RGB_G] = 0;
-    whiteCalibration.value[TCS230_RGB_B] = 0;
-
-    sensorData blackCalibration;
-    blackCalibration.value[TCS230_RGB_R] = 0;
-    blackCalibration.value[TCS230_RGB_G] = 0;
-    blackCalibration.value[TCS230_RGB_B] = 0;
-
-    colorSensor.begin();
-    colorSensor.setDarkCal(&blackCalibration);
-    colorSensor.setWhiteCal(&whiteCalibration);
-
-    pinMode(R_OUT, OUTPUT);
-    pinMode(G_OUT, OUTPUT);
-    pinMode(B_OUT, OUTPUT);
+    for(int i = startPin; i < endPin; i++){
+        pinMode(i, OUTPUT);
+        digitalWrite(i, HIGH);
+    }
 }
 
-void loop() 
-{
-    colorData rgb;
-    colorSensor.read();
-
-    while (!colorSensor.available());
-
-    colorSensor.getRGB(&rgb);
-    print_rgb(rgb);
-    set_rgb_led(rgb);
+void loop() {
+    if(Serial.available()){
+        int number = Serial.parseInt();
+        bool *desiredLEDs = get_array_whether_use_LED(number, countLed);
+        Serial.println(number);
+        light_off(startPin, endPin);
+        light_on(a, startPin, endPin);
+    }
 }
 
-void print_rgb(colorData rgb)
+void light_on(bool* desiredLEDs, int startPin, int endPin)
 {
-  Serial.print(rgb.value[TCS230_RGB_R]);
-  Serial.print(" ");
-  Serial.print(rgb.value[TCS230_RGB_G]);
-  Serial.print(" ");
-  Serial.print(rgb.value[TCS230_RGB_B]);
-  Serial.println();
+    for (int i = startPin; i < endPin; i++){
+        if (desiredLEDs[i - startPin]){
+            digitalWrite(i, LOW);
+        }
+    }
 }
 
-void set_rgb_led(colorData rgb)
+void light_off(int startPin, int endPin)
 {
-    analogWrite(R_OUT, 255 - rgb.value[TCS230_RGB_R]);
-    analogWrite(G_OUT, 255 - rgb.value[TCS230_RGB_G]);
-    analogWrite(B_OUT, 255 - rgb.value[TCS230_RGB_B]);
+    for (int i = startPin; i < endPin; i++){
+        digitalWrite(i, HIGH);
+    }
+}
+
+bool * get_array_whether_use_LED(int number, int numberBinaryDigits)
+{
+    bool* result = new bool[numberBinaryDigits];
+    for(int i = 0; i < numberBinaryDigits; i++)
+    {
+        result[i] = number % 2 == 1;
+        number /= 2;
+    }
+    return result;
 }
